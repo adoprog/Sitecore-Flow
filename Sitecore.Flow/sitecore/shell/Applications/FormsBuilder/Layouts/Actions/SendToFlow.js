dc7d6d4828f1382fc1d565346e089edfb49206b1
Alexander Doroshenko <ado@sitecore.net>
2018-02-28 17:42:23 +0200

    Added SC9 form action editor code

---
+++ Sitecore.Flow/sitecore/shell/Applications/FormsBuilder/Layouts/Actions/SendToFlow.js
@@ -0,0 +1,28 @@
+﻿(function (speak) {
+  var parentApp = window.parent.Sitecore.Speak.app.findApplication('EditActionSubAppRenderer');
+  speak.pageCode([], function () {
+    return {
+      initialized: function () {
+        this.on({
+          "loaded": this.loadDone
+        }, this);
+
+        if (parentApp) {
+          parentApp.loadDone(this, this.HeaderTitle.Text, this.HeaderSubtitle.Text);
+          debugger;
+          parentApp.setSelectability(this, true, null);
+        }
+      },
+
+      loadDone: function (parameters) {
+        this.Parameters = parameters || {};
+        this.TriggerAddress.Value = this.Parameters.triggerAddress;
+      },
+
+      getData: function () {
+        this.Parameters.triggerAddress = this.TriggerAddress.Value;
+        return this.Parameters;
+      }
+    };
+  });
+})(Sitecore.Speak);
